f6f7b2bb5b709d5749c048e0072b2a0671d5064b
Erwan <erwan.merly@etu.imt-lille-douai.fr>
2021-02-16 15:05:56 +0100

    Ajout exercice 5a

--- lib/exercice5a.dart
+++ lib/exercice5a.dart
@@ -22,9 +22,20 @@ class _Exercice5a_Page extends State<Exercice5a_Page> {
         margin: EdgeInsets.all(20.0),
         child: GridView.count(
           crossAxisCount: 3,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
           children: [
-            Container(child: Text("oui")),
-            Container(child: Text("oui")),
+            Container(color: Colors.deepPurple, child:Center(child: Text("Tile 1"))),
+            Container(color: Colors.blueGrey, child:Center(child: Text("Tile 2"))),
+            Container(color: Colors.grey, child:Center(child: Text("Tile 3"))),
+
+            Container(color: Colors.redAccent, child:Center(child: Text("Tile 4"))),
+            Container(color: Colors.amberAccent, child:Center(child: Text("Tile 5"))),
+            Container(color: Colors.cyanAccent, child:Center(child: Text("Tile 6"))),
+
+            Container(color: Colors.tealAccent, child:Center(child: Text("Tile 7"))),
+            Container(color: Colors.red, child:Center(child: Text("Tile 8"))),
+            Container(color: Colors.lightGreenAccent, child:Center(child: Text("Tile 9"))),
           ],
         ),
       ),
